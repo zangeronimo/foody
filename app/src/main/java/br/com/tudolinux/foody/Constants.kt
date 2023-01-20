64a393a2937959f062f21ae474736d4a7d687c36
Luciano Zangeronimo <zangeronimo@gmail.com>
2023-01-20 16:06:16 -0300

    Delete Constants.kt
    remove incorrect file

--- app/src/main/java/br/com/tudolinux/foody/Constants.kt
+++
@@ -1,9 +0,0 @@
-package br.com.tudolinux.foody
-
-class Constants {
-
-    companion object {
-        const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = "7bb64e48a59c4e0894ed6bab9dff9f70"
-    }
-}
